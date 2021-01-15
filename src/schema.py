bf7d9b376b679cbd9509a1fde6e18037db6abda6
Zachary Arnaise <57530191+zacharyarnaise@users.noreply.github.com>
2021-01-15 22:52:29 +0100

    Schema GraphQL fonctionnel (normalement...)

--- src/schema.py
+++ src/schema.py
@@ -3,22 +3,122 @@ Zachary Arnaise
 """
 
 import graphene
-from schema_types import PersonType
+
+from models import Movie, MoviePersons, Person
+from schema_types import MovieType, PersonType
 
 
 class Query(graphene.ObjectType):
     """Query, objet principal GraphQL.
+
+    Attributes:
+        person (graphene.Field): Champ permettant l'accès à une personne
+        via son ID, prénom ou nom.
+        movie (graphene.Field): Champ permettant l'accès à un film
+        via son ID, titre français, titre original ou status.
+
+        persons (graphene.List): Liste de toutes les personnes, sans filtrage.
+        movies (graphene.List): Liste de tous les films.
+        directors (graphene.List): Liste de toutes les personnes ayant au moins
+        réalisé un film.
+        actors (graphene.List): Liste de toutes les personnes ayant au moins
+        joué dans un film.
+        songWriters (graphene.List): Liste de toutes les personnes ayant au
+        moins composé la musique d'un film.
     """
 
-    person = graphene.Field(PersonType)
+    person = graphene.Field(
+        PersonType,
+        id=graphene.Argument(type=graphene.Int, required=False),
+        firstName=graphene.Argument(type=graphene.String, required=False),
+        lastName=graphene.Argument(type=graphene.String, required=False),
+    )
 
-    def resolve_person(parent: object, info: graphene.ResolveInfo):
-        return PersonType(id=1337, test="foobar")
+    movie = graphene.Field(
+        MovieType,
+        id=graphene.Argument(type=graphene.Int, required=False),
+        frenchTitle=graphene.Argument(type=graphene.String, required=False),
+        originalTitle=graphene.Argument(type=graphene.String, required=False),
+        status=graphene.Argument(type=graphene.String, required=False),
+    )
+
+    def resolve_person(parent: object, info: graphene.ResolveInfo, **kwargs):
+        """Méthode qui cherche une personne selon un ou plusieurs critères."""
+        query = PersonType.get_query(info=info)
+
+        if "id" in kwargs:
+            query = query.filter(Person.id == kwargs["id"])
+        if "firstName" in kwargs:
+            query = query.filter(Person.firstName == kwargs["firstName"])
+        if "lastName" in kwargs:
+            query = query.filter(Person.lastName == kwargs["lastName"])
+
+        return query.first()
+
+    def resolve_movie(parent: object, info: graphene.ResolveInfo, **kwargs):
+        """Méthode qui cherche un film selon un ou plusieurs critères."""
+        query = MovieType.get_query(info=info)
+
+        if "id" in kwargs:
+            query = query.filter(Movie.id == kwargs["id"])
+        if "frenchTitle" in kwargs:
+            query = query.filter(Movie.frenchTitle == kwargs["frenchTitle"])
+        if "originalTitle" in kwargs:
+            query = query.filter(Movie.frenchTitle == kwargs["originalTitle"])
+        if "status" in kwargs:
+            query = query.filter(Movie.status == kwargs["status"])
+
+        return query.first()
+
+    persons = graphene.List(PersonType)
+    movies = graphene.List(MovieType)
+    directors = graphene.List(PersonType)
+    actors = graphene.List(PersonType)
+    songWriters = graphene.List(PersonType)
+
+    def resolve_persons(root: object, info: graphene.ResolveInfo, **kwargs):
+        query = PersonType.get_query(info=info)
+        return query.all()
+
+    def resolve_movies(root: object, info: graphene.ResolveInfo, **kwargs):
+        query = MovieType.get_query(info=info)
+        return query.all()
+
+    def resolve_directors(root: object, info: graphene.ResolveInfo, **kwargs):
+        session = info.context["session"]
+        return [
+            res.person
+            for res in session.query(MoviePersons)
+            .filter_by(person_role_id=1)
+            .group_by(MoviePersons.person_id)
+            .all()
+        ]
+
+    def resolve_actors(root: object, info: graphene.ResolveInfo, **kwargs):
+        session = info.context["session"]
+        return [
+            res.person
+            for res in session.query(MoviePersons)
+            .filter_by(person_role_id=2)
+            .group_by(MoviePersons.person_id)
+            .all()
+        ]
+
+    def resolve_songWriters(root: object, info: graphene.ResolveInfo, **kwargs):
+        session = info.context["session"]
+        return [
+            res.person
+            for res in session.query(MoviePersons)
+            .filter_by(person_role_id=3)
+            .group_by(MoviePersons.person_id)
+            .all()
+        ]
 
 
 schema = graphene.Schema(
     query=Query,
     types=[
         PersonType,
+        MovieType,
     ],
 )
--- src/schema_types.py
+++ src/schema_types.py
@@ -3,8 +3,105 @@ Zachary Arnaise
 """
 
 import graphene
+from graphene_sqlalchemy import SQLAlchemyObjectType
+
+from models import Movie, Person
 
 
-class PersonType(graphene.ObjectType):
-    id = graphene.ID()
-    test = graphene.String()
+class PersonType(SQLAlchemyObjectType):
+    """
+    type Person {
+        id : ID
+        firstName : String
+        lastName : String
+        fullName : String
+        dateOfBirth : Date
+        dateOfDeath : Date
+        directed : [Movie]
+        playedIn : [Movie]
+        composed : [Movie]
+    }
+    """
+
+    class Meta:
+        model = Person
+
+    fullName = graphene.String()
+    directed = graphene.List(of_type=lambda: MovieType)
+    playedIn = graphene.List(of_type=lambda: MovieType)
+    composed = graphene.List(of_type=lambda: MovieType)
+
+    def resolve_fullName(parent: object, info: graphene.ResolveInfo):
+        # Nom complet à partir du prénom + nom de famille
+        return f"{parent.firstName} {parent.lastName}"
+
+    def resolve_directed(parent: object, info: graphene.ResolveInfo):
+        return [
+            assoc.movie
+            for assoc in parent.career
+            if assoc.role.description == "réalisateur"
+        ]
+
+    def resolve_playedIn(parent: object, info: graphene.ResolveInfo):
+        return [
+            assoc.movie for assoc in parent.career if assoc.role.description == "acteur"
+        ]
+
+    def resolve_composed(parent: object, info: graphene.ResolveInfo):
+        return [
+            assoc.movie
+            for assoc in parent.career
+            if assoc.role.description == "compositeur"
+        ]
+
+
+class MovieType(SQLAlchemyObjectType):
+    """
+    type Movie {
+        id : ID
+        frenchTitle : String
+        originalTitle : String
+        status : String
+        statusDate : Date
+        directors : [Person]
+        actors : [Person]
+        songWriters : [Person]
+    }
+    """
+
+    class Meta:
+        model = Movie
+        fields = (
+            "id",
+            "frenchTitle",
+            "originalTitle",
+            "statusDate",
+        )
+        exclude_fields = ("statusId",)
+
+    status = graphene.String()
+    directors = graphene.List(of_type=PersonType)
+    actors = graphene.List(of_type=PersonType)
+    songWriters = graphene.List(of_type=PersonType)
+
+    def resolve_status(parent: object, info: graphene.ResolveInfo):
+        return parent.status.description
+
+    def resolve_directors(parent: object, info: graphene.ResolveInfo):
+        return [
+            assoc.person
+            for assoc in parent.crew
+            if assoc.role.description == "réalisateur"
+        ]
+
+    def resolve_actors(parent: object, info: graphene.ResolveInfo):
+        return [
+            assoc.person for assoc in parent.crew if assoc.role.description == "acteur"
+        ]
+
+    def resolve_songWriters(parent: object, info: graphene.ResolveInfo):
+        return [
+            assoc.person
+            for assoc in parent.crew
+            if assoc.role.description == "compositeur"
+        ]
