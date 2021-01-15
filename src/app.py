e6fc6d44a91ed536d50814562320022990b84eaf
Zachary Arnaise <57530191+zacharyarnaise@users.noreply.github.com>
2021-01-15 15:24:48 +0100

    Test endpoint GraphQL

--- src/app.py
+++ src/app.py
@@ -5,11 +5,16 @@ Zachary Arnaise
 import falcon
 import sqlalchemy.orm
 
+from resources import ResourceGraphQL
 
 # Init ORM
-engine = sqlalchemy.create_engine("sqlite:///movies.db", echo=True)
+engine = sqlalchemy.create_engine("sqlite:///../movies.db", echo=True)
 sessionmaker = sqlalchemy.orm.sessionmaker(bind=engine)
 scoped_session = sqlalchemy.orm.scoped_session(sessionmaker)
 
 # Init serveur Falcon
 app = falcon.API()
+app.add_route(
+    uri_template="/graphql",
+    resource=ResourceGraphQL(scoped_session=scoped_session),
+)
