2a8b0928caa8bdf61507d646f0569ffa51f5aacf
Zachary Arnaise <57530191+zacharyarnaise@users.noreply.github.com>
2021-01-15 15:12:49 +0100

    Init ORM + Falcon

--- src/app.py
+++ src/app.py
@@ -1,3 +1,15 @@
 """ LPGL - IUT Metz
 Zachary Arnaise
 """
+
+import falcon
+import sqlalchemy.orm
+
+
+# Init ORM
+engine = sqlalchemy.create_engine("sqlite:///movies.db", echo=True)
+sessionmaker = sqlalchemy.orm.sessionmaker(bind=engine)
+scoped_session = sqlalchemy.orm.scoped_session(sessionmaker)
+
+# Init serveur Falcon
+app = falcon.API()
