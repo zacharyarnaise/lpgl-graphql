0ba5cf982bee4f789a2de691ca3132f20cccbc6d
Zachary Arnaise <57530191+zacharyarnaise@users.noreply.github.com>
2021-01-16 01:19:59 +0100

    Vérif. fichier SQLite avant lancement

--- src/app.py
+++ src/app.py
@@ -2,11 +2,18 @@
 Zachary Arnaise
 """
 
+import sys
+import os.path
 import falcon
 import sqlalchemy.orm
 
 from resources import ResourceGraphQL
 from schema import schema
+
+
+if not os.path.isfile("/db/movies.db"):
+    print("ERREUR: Accès à la db SQLite impossible, aucun fichier trouvé.")
+    sys.exit(1)
 
 # Init ORM
 engine = sqlalchemy.create_engine("sqlite:////db/movies.db", echo=True)
